8e11e711e0b816c353cb80121752222c5bc7a507
Dfedotova <dariafedotova42@gmail.com>
2023-03-13 13:00:37 +0300

    Cleaning

--- lib/widgets/tasks/task_card_widget.dart
+++ lib/widgets/tasks/task_card_widget.dart
@@ -1,8 +1,6 @@
 import 'dart:ui';
 
 import 'package:crnt_task/controllers/dialogue_windows_controller.dart';
-import 'package:crnt_task/data/skills.dart';
-import 'package:crnt_task/models/skill.dart';
 import 'package:crnt_task/widgets/skills/active_skill_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
