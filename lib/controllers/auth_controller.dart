385657be98de794c57b22d1ab4a741d9168fd7a2
Dfedotova <dariafedotova42@gmail.com>
2023-03-18 20:26:07 +0300

    Minimal auth done

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,4 @@
+class AuthController{
+  static const String login = 'dariafedotova42@gmail.com';
+  static const String password = 'abc123';
+}
--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-import 'package:crnt_task/controllers/main_page_controller.dart';
+import 'package:crnt_task/controllers/auth_controller.dart';
 import 'package:crnt_task/locator.dart';
 import 'package:crnt_task/navigation/navigation_service.dart';
 import 'package:crnt_task/navigation/route_names.dart';
@@ -16,25 +16,18 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-  final MainPageController c = Get.put(MainPageController());
-
-  bool _initialized = false;
+  final TextEditingController _loginController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  late String _inputLogin;
+  late String _inputPassword;
+  final RxBool _authFailed = false.obs;
 
   @override
   void initState() {
     super.initState();
   }
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (!_initialized) {
-      _initialized = true;
-      c.load();
-    }
-  }
-
-  Widget _textFieldWidget(String hint) {
+  Widget _textFieldWidget(String hint, TextEditingController controller) {
     return Container(
       padding: const EdgeInsets.only(left: 20),
       width: double.infinity,
@@ -46,14 +39,15 @@ class _AuthPageState extends State<AuthPage> {
       child: Align(
         alignment: Alignment.centerLeft,
         child: TextFormField(
+          controller: controller,
           style: TextStyle(
             height: 1.2,
             fontSize: 16,
             fontWeight: FontWeight.w300,
             fontFamily: 'Montserrat',
-            color: Theme.of(context).colorScheme.surface,
+            color: Theme.of(context).colorScheme.primary,
           ),
-          cursorColor: Theme.of(context).colorScheme.scrim,
+          cursorColor: Theme.of(context).colorScheme.primary,
           cursorHeight: 20,
           decoration: InputDecoration.collapsed(
             hintStyle: TextStyle(
@@ -65,6 +59,13 @@ class _AuthPageState extends State<AuthPage> {
             ),
             hintText: hint,
           ),
+          onChanged: (value) {
+            if (hint == 'Login') {
+              _inputLogin = value;
+            } else {
+              _inputPassword = value;
+            }
+          },
         ),
       ),
     );
@@ -107,46 +108,79 @@ class _AuthPageState extends State<AuthPage> {
                                 .withOpacity(0.25),
                           ),
                           child: Padding(
-                            padding: const EdgeInsets.all(59),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  'вход',
-                                  style: TextStyle(
-                                    inherit: false,
-                                    height: 0.75,
-                                    fontFamily: 'Druk Wide Cyr',
-                                    fontSize: 60,
-                                  ),
-                                ),
-                                const SizedBox(height: 30),
-                                _textFieldWidget('Login'),
-                                const SizedBox(height: 19),
-                                _textFieldWidget('Password'),
-                                const SizedBox(height: 40),
-                                GestureDetector(
-                                  onTap: () {
-                                    locator<NavigationService>()
-                                        .navigateTo(HomeRoute);
-                                  },
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    height: 50,
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .secondary,
-                                      borderRadius: BorderRadius.circular(30),
-                                    ),
-                                    child: Text(
-                                      'Войти',
-                                      style: Theme.of(context).textTheme.button,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 59,
+                              vertical: 40,
+                            ),
+                            child: Obx(
+                              () => Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'вход',
+                                    style: TextStyle(
+                                      inherit: false,
+                                      height: 0.75,
+                                      fontFamily: 'Druk Wide Cyr',
+                                      fontSize: 60,
                                     ),
                                   ),
-                                ),
-                              ],
+                                  const SizedBox(height: 35),
+                                  _textFieldWidget(
+                                    'Login',
+                                    _loginController,
+                                  ),
+                                  const SizedBox(height: 19),
+                                  _textFieldWidget(
+                                    'Password',
+                                    _passwordController,
+                                  ),
+                                  if (_authFailed.value)
+                                    const Padding(
+                                      padding: EdgeInsets.only(left: 10,top: 25),
+                                      child: Text(
+                                        '* Неверный логин или пароль',
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    )
+                                  else
+                                    const SizedBox(),
+                                  const Spacer(),
+                                  GestureDetector(
+                                    onTap: () {
+                                      if (_inputLogin == AuthController.login &&
+                                          _inputPassword ==
+                                              AuthController.password) {
+                                        _authFailed.value = false;
+                                        locator<NavigationService>()
+                                            .navigateTo(HomeRoute);
+                                      } else {
+                                        _authFailed.value = true;
+                                      }
+                                    },
+                                    child: Container(
+                                      alignment: Alignment.center,
+                                      height: 50,
+                                      width: double.infinity,
+                                      decoration: BoxDecoration(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                        borderRadius: BorderRadius.circular(30),
+                                      ),
+                                      child: Text(
+                                        'Войти',
+                                        style:
+                                            Theme.of(context).textTheme.button,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
