061bf8a5eab00f3cf06d98b27af6cd893ec611ad
Dfedotova <dariafedotova42@gmail.com>
2023-04-18 00:08:27 +0300

    pkg deleted

--- crnt-data-manager/example/crnt_data_manager_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:crnt_data_manager/crnt_data_manager.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- crnt-data-manager/lib/crnt_data_manager.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library crnt_data_manager;
-
-export 'src/crnt_data_manager_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
